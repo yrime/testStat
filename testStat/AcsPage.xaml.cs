5bd64317887f18665f35233eba4ad5e2a5fb9556
Феликс Родионов < >
2016-12-03 19:44:28 +0300

    Adding interface and handling classes

--- testStat/AcsPage.xaml.cs
+++ testStat/AcsPage.xaml.cs
@@ -7,14 +7,193 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using Microsoft.Devices.Sensors;
+using System.Diagnostics;
+using System.Threading;
+using System.IO.IsolatedStorage;
+using System.Windows.Input;
+using System.Xml.Serialization;
 
 namespace testStat
 {
     public partial class AcsPage : PhoneApplicationPage
     {
+        ReaderWriterLockSlim locker;
+        ManualResetEventSlim slim;
+        bool end = false;
+        bool window = true;
+
+        int StatIteration = 0;
+        const int namberOfIteration = 10;
+
+        List<buffer> bufferList = new List<buffer>();
+
+        Accelerometer myAcs;
+        bufAcs bA;
+        bufScr bC;
+        Thread stData;
+
+        IsolatedStorageFile file;
+        IsolatedStorageFileStream fileStream;
+
+        System.IO.StreamWriter sw;
+        XmlSerializer seriXML = new XmlSerializer(typeof(buffer));
+
+        bool sttoper = false;
+
         public AcsPage()
         {
             InitializeComponent();
+
+            file = IsolatedStorageFile.GetUserStoreForApplication();
+            
         }
+
+        private void ok_Click(object sender, RoutedEventArgs e)
+        {
+            messageDialog.IsOpen = false;
+            this.sttoper = true;
+        }
+
+        private void messageDialog_Closed(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void LayoutRoot_ManipulationCompleted(object sender, System.Windows.Input.ManipulationCompletedEventArgs e)
+        {
+            if ((myAcs != null) && this.sttoper)
+            {
+                myAcs.Stop();
+                myAcs.Dispose();
+                myAcs = null;
+                messageDialog.IsOpen = true;
+                end = true;
+                sw.Close();
+                if (StatIteration > namberOfIteration)
+                {
+                    this.sttoper = false;
+                    StatIteration = 0;
+                    
+                }
+            }
+        }
+
+        private void LayoutRoot_ManipulationDelta(object sender, System.Windows.Input.ManipulationDeltaEventArgs e)
+        {
+            if (this.sttoper)
+            {
+                bC = new bufScr((int)e.ManipulationOrigin.X, (int)e.ManipulationOrigin.Y);
+            }
+        }
+
+        private void LayoutRoot_ManipulationStarted(object sender, System.Windows.Input.ManipulationStartedEventArgs e)
+        {
+            if (this.sttoper)
+            {
+                bC = new bufScr((int)e.ManipulationOrigin.X, (int)e.ManipulationOrigin.Y);
+                if (myAcs == null)
+                {
+                    myAcs = new Accelerometer();
+                    myAcs.ReadingChanged +=
+                        new EventHandler<AccelerometerReadingEventArgs>(myAcs_readingChanged);
+                    myAcs.Start();
+                }
+                else
+                {
+                    myAcs.Stop();
+                    myAcs.Dispose();
+                    myAcs = null;
+                }
+
+                end = false;
+
+                stData = new Thread(new ThreadStart(ThreadTask));
+                stData.Start();
+            }
+        }
+        private void ThreadTask()
+        {
+            buffer b;
+            int sleep = 50;
+            int time = 0;
+    /*        Dispatcher.BeginInvoke(() =>
+            {
+                meTBox.Text = " hgjh";
+            });
+    */      while (!end)
+            {
+                b = new buffer();
+                b.time = time;
+                b.acs = bA;
+                b.scr = bC;
+                seriXML.Serialize(sw, b);
+                Thread.Sleep(sleep);
+                time += sleep;
+            }
+        }
+        private void myAcs_readingChanged(object sender, AccelerometerReadingEventArgs e)
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(() => my_readingChanged(e));
+        }
+        /*
+         * 
+         * Чтение и запись значений акселерометра в файл
+         * 
+         */
+        private void my_readingChanged(AccelerometerReadingEventArgs e)
+        {
+            if (myAcs != null)
+            {
+                bA = new bufAcs((float)e.X, (float)e.Y, (float)e.Z);
+            }
+        }
+
+        private void meTBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (window)
+            {
+                string outData = NavigationContext.QueryString["UserString"].ToString();
+                if (!file.FileExists(outData+".txt"))
+                {
+                    fileStream = file.CreateFile(outData + ".txt");
+                }
+                else
+                {
+                    fileStream = file.OpenFile(outData + ".txt", System.IO.FileMode.Open);
+                }
+                sw = new System.IO.StreamWriter(fileStream);
+                window = false;
+            }
+            
+        }
+    }
+    public struct bufAcs
+    {
+        public float x;
+        public float y;
+        public float z;
+        public bufAcs(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+    public struct bufScr
+    {
+        public int x;
+        public int y;
+        public bufScr(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public struct buffer
+    {
+        public double time;
+        public bufAcs acs;
+        public bufScr scr;
     }
 }
--- testStat/MainPage.xaml.cs
+++ testStat/MainPage.xaml.cs
@@ -14,7 +14,7 @@ namespace testStat
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        //
+        //for texblock, placeholder  будет появляться еслт поле ввода пустое
 
         private string placeholder = "Ваше имя";
 
@@ -31,190 +31,16 @@ namespace testStat
             }
         }
 
-        //vars
-        //List<String> myList = new List<String>();
-        XmlSerializer seriXML = new XmlSerializer(typeof(buffer));
-        List<buffer> bufferList = new List<buffer>();
-
-        Accelerometer myAcs;
-        Thread stData;
-
-        String outData = "";
-        bool userNameEntered = false;
-        int StatIteration = 0;
-
-        bufAcs bA = new bufAcs();
-        bufScr bS = new bufScr();
-
-        const int namberOfIteration = 10;
-
-        TextBox uName;
-        IsolatedStorageFile file;
-        IsolatedStorageFileStream fileStream;
-
-        System.IO.StreamWriter sw;
-
         // Constructor
         public MainPage()
         {
             InitializeComponent();
-
-            file = IsolatedStorageFile.GetUserStoreForApplication();
-
         }
-
-        //sreen tap does complete
-        private void LayoutRoot_ManipulationCompleted(object sender, System.Windows.Input.ManipulationCompletedEventArgs e)
-        {
-            
-            //stData.Abort();
-            bufferList.Add(new buffer());
-            if (myAcs != null)
-            {
-                myAcs.Stop();
-                myAcs.Dispose();
-                myAcs = null;
-                ++StatIteration;
-
-                if (StatIteration > namberOfIteration)
-                {
-                    StatIteration = 0;
-                    userNameEntered = false;
-                }
-            }
-        }
-
-
-        //return hold screen coordinates
-        private void LayoutRoot_ManipulationDelta(object sender, System.Windows.Input.ManipulationDeltaEventArgs e)
-        {
-            if (userNameEntered)
-            {
-                Point rp = ((UIElement)e.OriginalSource).TransformToVisual(LayoutRoot).Transform(new Point(0, 0));
-                outData = (rp.X + e.ManipulationOrigin.X).ToString() + " " + (rp.Y + e.ManipulationOrigin.Y).ToString();
-                bS.x = (int)(rp.X + e.ManipulationOrigin.X);
-                bS.y = (int)(rp.Y + e.ManipulationOrigin.Y);
-            }
-        }
-        //return start 9screen tap cordinates
-        /**
-         * 
-         * create new process for acselerometr?
-         * NO
-         * 
-         */
-        private void LayoutRoot_ManipulationStarted(object sender, System.Windows.Input.ManipulationStartedEventArgs e)
-        {
-            if (userNameEntered)
-            {
-                Point rp = ((UIElement)e.OriginalSource).TransformToVisual(LayoutRoot).Transform(new Point(0, 0));
-                // outData = (rp.X + e.ManipulationOrigin.X).ToString() + " " + (rp.Y + e.ManipulationOrigin.Y).ToString();
-                bS.x = (int)(rp.X + e.ManipulationOrigin.X);
-                bS.y = (int)(rp.Y + e.ManipulationOrigin.Y);
-
-                if (myAcs == null)
-                {
-                    myAcs = new Accelerometer();
-                    myAcs.ReadingChanged +=
-                        new EventHandler<AccelerometerReadingEventArgs>(myAcs_readingChanged);
-                    myAcs.Start();
-                }
-                else
-                {
-                    myAcs.Stop();
-                    myAcs.Dispose();
-                    myAcs = null;
-                }
-                stData = new Thread(new ThreadStart(ThreadTask));
-                stData.Start();
-            }
-        }
-
-        private void ThreadTask()
-        { 
-            Stopwatch st = new Stopwatch();
-            buffer b;
-            st.Start();
-            while (true)
-            {
-                b = new buffer();
-                b.time = st.Elapsed.Milliseconds;
-                b.acs = new bufAcs(bA);
-                b.scr = new bufScr(bS);
-                seriXML.Serialize(sw, b);
-            }
-        }
-
-        private void myAcs_readingChanged(object sender, AccelerometerReadingEventArgs e)
-        {
-            Deployment.Current.Dispatcher.BeginInvoke(() => my_readingChanged(e));
-        }
-        /*
-         * 
-         * Чтение и запись значений акселерометра в файл
-         * 
-         */
-        private void my_readingChanged(AccelerometerReadingEventArgs e)
-        {
-            if (myAcs != null)
-            {
-               
-                bA.x = (float)e.X;
-                bA.y = (float)e.Y;
-                bA.z = (float)e.Z;               
-            }
-        }
-
-        /*
-         * 
-         * Обработка ввода по нажатию enter
-         * 
-         */
-
-        private void TextBox_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                userNameEntered = true;
-                this.Focus();
-
-                outData = "" + uName.Text.ToString();
-                outData = outData.Substring(0, outData.Length - 2);
-                outData += ".txt";
-
-                if (!file.FileExists(outData))
-                {
-                    fileStream = file.CreateFile(outData);
-                }else
-                {
-                    fileStream = file.OpenFile(outData, System.IO.FileMode.Open);
-                }
-
-                sw = new System.IO.StreamWriter(fileStream);
-            }
-        }
-
-
-    /*    private void stoper_Click(object sender, RoutedEventArgs e)
-        {
-            sw.Close();
-            fileStream.Close();
-            stData.Abort();
-            if (myAcs != null)
-            {
-                myAcs.Stop();
-                myAcs.Dispose();
-                myAcs = null;
-            }
-
-            Application.Current.Terminate();
-        }
-        */
 
         private void btn_name_enter(object sender, RoutedEventArgs e)
         {
 
-            NavigationService.Navigate(new Uri("/AcsPage.xaml", UriKind.Relative));
+            NavigationService.Navigate(new Uri(string.Format("/AcsPage.xaml?UserString={0}",tbMain.Text), UriKind.Relative));
         }
 
         private void tbMain_GotFocus(object sender, RoutedEventArgs e)
@@ -241,35 +67,5 @@ namespace testStat
             tbMain.Focus();
         }
 
-    }
-
-    public struct bufAcs
-    {
-        public float x;
-        public float y;
-        public float z;
-        public bufAcs(bufAcs a)
-        {
-            x = a.x;
-            y = a.y;
-            z = a.z;
-        }
-    }
-
-    public struct bufScr
-    {
-        public int x;
-        public int y;
-        public bufScr(bufScr a)
-        {
-            x = a.x;
-            y = a.y;
-        }
-    }
-    public struct buffer
-    {
-        public double time;
-        public bufAcs acs;
-        public bufScr scr;
     }
 }
